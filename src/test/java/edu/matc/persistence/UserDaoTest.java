5b8c30c1d8484a2fc43f1ad51ef6744cac8fb39f
Matt Brophy <mattbroph@gmail.com>
2025-02-11 19:19:38 -0600

    added getByID() & insert() test, finished video 2 of demo

---
+++ src/test/java/edu/matc/persistence/UserDaoTest.java
@@ -0,0 +1,63 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.User;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    // Step 1 is always create the class / thing we want to test
+    UserDao userDao;
+
+    @Test
+    void getById() {
+
+        userDao = new UserDao();
+        User retrievedUser = userDao.getById(1);
+        assertNotNull(retrievedUser);
+        assertEquals("Joe", retrievedUser.getFirstName());
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void insert() {
+
+        int userId = 0;
+
+        userDao = new UserDao();
+        User myUser = new User("Mary", "Smith",
+                "marySmith", LocalDate.of(1900, 1, 1));
+        userId = userDao.insert(myUser);
+
+        assertNotEquals(0, userId);
+
+        User inserteredUser = userDao.getById(userId);
+
+        assertEquals("Mary", inserteredUser.getFirstName());
+
+
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByPropertyEqual() {
+    }
+
+    @Test
+    void getByPropertyLike() {
+    }
+}
